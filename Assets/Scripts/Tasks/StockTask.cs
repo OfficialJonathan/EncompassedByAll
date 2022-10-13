bac531521039c48eee9f3ee8a5949aa82f0447de
OfficialJonathan <90244872+OfficialJonathan@users.noreply.github.com>
2022-10-13 11:46:17 +0200

    Vending sprite

---
+++ Assets/Scripts/Tasks/StockTask.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StockTask : Task
+{
+    protected override void OnStart()
+    {
+
+    }
+
+    public override void OnInteract()
+    {
+
+    }
+}
